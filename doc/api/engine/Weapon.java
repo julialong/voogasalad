959e979188758f56301544d8aad1428d6cd2656d
Marcus Oertle <mlo11@duke.edu>
2018-03-27 19:34:15 -0400

    updated weapon, projectile, and powerup interfaces

---
+++ doc/api/engine/Weapon.java
@@ -0,0 +1,17 @@
+/**
+ * Interface for weapon type classes that can be utilized by either the player or the enemy classes.
+ * @author Marcus
+ *
+ */
+public interface Weapon {
+	/**
+	 * Sets the attack method type for a weapon (ie. swing, stab, shoot, etc...)
+	 * @param method - the string representation of the desired attack method
+	 */
+	public abstract void setAttackMethod(String method);
+	
+	/**
+	 * Causes the weapon to attack
+	 */
+	public abstract void attack();
+}
