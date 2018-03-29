75ad85aaf4ce80bf4950dc8f0f94a028dcd46ec3
Maya Messinger <maya.messinger@duke.edu>
2018-03-29 17:14:56 -0400

    Revert "Added data code to AddBlockDuringGame"
    This reverts commit 3059d3e09bb5821c8d1987bdea28b0d28ed3a4a2.

--- doc/usecases/AddBlockDuringGame.java
+++ doc/usecases/AddBlockDuringGame.java
@@ -1,40 +1,5 @@
 package usecases;
 
-import api.data.GAEtoJSON;
-import api.data.GEtoJSON;
-import api.data.JSONtoObject;
-import api.gae.GameCreator;
-import api.player.GameView;
-import usecases.sample_object_classes.PlatformGameCreator;
-
 public class AddBlockDuringGame {
-	GameCreator myGameCreator;
-	GameView myGameView;
-	EngineManager myEngineManager;
-	GAEtoJSON myGAEToJson;
-	GEtoJSON myGEToJson;
-	JSONtoObject myJSONToObj;
-	GameLevel myGameLevel;
 
-	public void addBlock()	{
-		// save current state of game to return to
-		myDataToObj.saveData(myGameLevel, myGameLevel.objects);
-		// make new instance of GAE to add objects
-		myGameCreator = new GameCreator();
-
-		// myGameCreator.addObject(new Block());
-
-		myGameCreator.saveGame();
-
-		// myGameCreator.close();
-	}
-
-
-}
-
-public class GameLevel	{
-	List<Object> objects;
-
-	public GameLevel()	{
-	}
 }
