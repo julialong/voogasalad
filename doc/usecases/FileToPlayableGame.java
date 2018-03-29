8b5c2a13e6272e0e977b4707ea0b1f56304612e0
Belanie Nagiel <bn51@duke.edu>
2018-03-29 16:57:41 -0400

    use case for file to playable game

--- doc/usecases/FileToPlayableGame.java
+++ doc/usecases/FileToPlayableGame.java
@@ -1,5 +1,28 @@
 package usecases;
 
+import api.data.JSONtoObject;
+
 public class FileToPlayableGame {
+	
+	/*
+	 * Creates a new instance of the JSONtoObject interface that will facilitate taking 
+	 * information from the database and parsing it into data that can be used by the 
+	 * Game Engine
+	 */
+	JSONtoObject myGameLoader;
+	
+	/**
+	 * Given a choice of game, calls the load existing function in the
+	 * game loader which returns a list of game objects that the game
+	 * engine can arrange into a playable game. This method would be called 
+	 * by game player when the user decides on a game they would like to play.
+	 * 
+	 * @param gameName Name of the game that is being loaded
+	 * @return a list of game objects to be arranged in the game engine
+	 */
+	List<Object> loadGameForPlay(String gameName)
+	{
+		return myGameLoader.loadExisting(gameName);
+	}
 
 }
