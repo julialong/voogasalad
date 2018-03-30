db0d4ec79dfe5c6f0c304c93a31f7c8ae987579d
Julia Long <julia.long@duke.edu>
2018-03-30 13:11:43 -0400

    Add EditorMain and EditorWindow classes

---
+++ src/EditorMain.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class is the entry point to the Graphic Authoring Environment and
+ * launches the necessary classes to start game editing
+ *
+ * @author julialong
+ */
+public class EditorMain extends Application{
+
+    /**
+     * Calls the necessary classes to open the editing environment
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    /**
+     * Runs the Editing environment
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/authoring_environment/EditorWindow.java
@@ -0,0 +1,27 @@
+package authoring_environment;
+
+/**
+ * EditorWindow implements the CreatorView interface and manages the
+ */
+public class EditorWindow implements CreatorView {
+
+    @Override
+    public void updateGrid() {
+
+    }
+
+    @Override
+    public void updateGridView(double location) {
+
+    }
+
+    @Override
+    public void openAttributeEditor(AuthoredElement elem) {
+
+    }
+
+    @Override
+    public void openNewWindow() {
+
+    }
+}
