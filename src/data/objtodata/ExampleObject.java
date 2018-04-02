244e3e590136ad3ddc253656df5b87fca1ccfc19
Maya Messinger <maya.messinger@duke.edu>
2018-04-02 16:25:23 -0400

    Somehow got some serialization to work

--- src/data/objtodata/ExampleObject.java
+++ src/data/objtodata/ExampleObject.java
@@ -1,11 +1,25 @@
 package data.objtodata;
 
-public class ExampleObject	{
-    int id;
-    public Class myClass = this.getClass();
-    private boolean canRemove = false;
+public class ExampleObject {  
+    String name;
+    String email;
+    int age;
+    boolean isDeveloper;
 
-    public ExampleObject(int id)	{
-        this.id = id;
+    public ExampleObject(String n, String e, int a, boolean i)	{
+    	name = n;
+    	email = e;
+    	age = a;
+    	isDeveloper = i;
     }
 }
+
+// public class ExampleObject	{
+//     int id;
+//     public Class myClass = this.getClass();
+//     private boolean canRemove = false;
+
+//     public ExampleObject(int id)	{
+//         this.id = id;
+//     }
+// }
--- src/data/objtodata/Tester.java
+++ src/data/objtodata/Tester.java
@@ -1,10 +1,19 @@
 package data.objtodata;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Tester	{
 	public static void main(String[] args)	{
-        Serializer aSer = new Serializer();
-        ExampleObject obj = new ExampleObject(12);
+		Gson gson = new Gson(); // new GsonBuilder().registerTypeAdapter(ExampleObject.class, new ExampleObjectSerializer()).create();
+        // ExampleObjectSerializer aSer = new ExampleObjectSerializer();
+        ExampleObject obj = new ExampleObject(
+				"Norman",
+				"norman@futurestud.io",
+				26,
+				true
+		);
 
-        aSer.serialize();
+        System.out.println(gson.toJson(obj));
 	}
 }
