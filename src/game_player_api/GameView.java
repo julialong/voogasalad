bc2901734bc714eff65b4a4722121615e1dbbae3
KELLEY SCROGGS <jks38@duke.edu>
2018-04-03 18:02:51 -0400

    dummy app

---
+++ src/game_player_api/GameView.java
@@ -0,0 +1,20 @@
+package game_player_api;
+
+import javafx.scene.Node;
+
+/**
+ * Defines the GameView, a GUI object that acts as the screen for the user to
+ * play the game on.
+ */
+public interface GameView {
+	/**
+	 * Returns the GameView node so it can be attached to the game player GUI.
+	 */
+	public Node getNode();
+
+	/*
+	 * Updates the game based on the result of passing the current game state and
+	 * user input to the game engine.
+	 */
+	public void updateGame();
+}
