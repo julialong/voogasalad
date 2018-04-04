99b62f0770f5c47d7e0b4c9e72f36c9e1ad0e2ad
Julia Long <julia.long@duke.edu>
2018-04-03 22:39:48 -0400

    Added level upload button

--- src/authoring_environment/editor_windows/LevelCreator.java
+++ src/authoring_environment/editor_windows/LevelCreator.java
@@ -6,7 +6,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 /**
  * The LevelCreator is a separate window that manages the creation of new levels within the game.
@@ -19,14 +22,19 @@ public class LevelCreator {
     private Stage myStage;
     private Scene myScene;
     private BorderPane myRoot;
+    // private Level newLevel;
+
+    File selectedImageFile;
 
     private static final String LEVEL_NAME = "Level name";
     private static final String SET_NAME = "Set name";
+    private static final String UPLOAD_BACKGROUND_IMAGE = "Upload background image";
 
     /**
      * Creates and launches a new LevelCreator window
      */
     public LevelCreator() {
+        createNewLevel();
         myStage = new Stage();
         myRoot = new BorderPane();
         addFields();
@@ -39,13 +47,20 @@ public class LevelCreator {
         myStage.centerOnScreen();
     }
 
+    private void createNewLevel() {
+        // TODO: create New level object
+        // newLevel = new BasicLevel();
+    }
+
     private void addFields() {
         myRoot.setTop(createTop());
+        myRoot.setRight(createRight());
+        myRoot.setCenter(createCenter());
     }
 
     private HBox createTop() {
         HBox box = new HBox();
-        box.getStyleClass().add("level-creator");
+        box.getStyleClass().add("level-top");
         Label instruction = new Label(LEVEL_NAME);
         box.getChildren().add(instruction);
         TextField name = new TextField();
@@ -55,11 +70,18 @@ public class LevelCreator {
     }
 
     private HBox createRight() {
-        return new HBox();
+        HBox box = new HBox();
+        box.getStyleClass().add("level-right");
+        Button uploadImageButton = createUploadImageButton();
+        box.getChildren().add(uploadImageButton);
+        return box;
     }
 
     private HBox createCenter() {
-        return new HBox();
+        HBox box = new HBox();
+        box.getStyleClass().add("level-center");
+        // TODO: show level display
+        return box;
     }
 
     private void createSubmitButton(Pane box, TextField name, Label instruction) {
@@ -67,8 +89,22 @@ public class LevelCreator {
         submitButton.setOnAction(e -> {
             box.getChildren().removeAll(name,submitButton,instruction);
             box.getChildren().add(new Text(name.getText()));
+            // TODO: assign the text as the Level object name
+            // newLevel.setName(name.getText());
         });
         box.getChildren().add(submitButton);
+    }
+
+    private Button createUploadImageButton() {
+        Button backgroundImage = new Button(UPLOAD_BACKGROUND_IMAGE);
+        backgroundImage.setOnAction(e -> {
+            FileChooser imageChooser = new FileChooser();
+            imageChooser.getExtensionFilters().add(
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"));
+            selectedImageFile =  imageChooser.showOpenDialog(myStage);
+            // TODO: set Level background to file
+        });
+        return backgroundImage;
     }
 
 }
