e88004142824ca484c04ac56272f41b5138a863d
Maya Messinger <maya.messinger@duke.edu>
2018-04-05 14:48:08 -0400

    Large internal overhaul of serialization, refactoring and taking out changed/removed items, will just take in whole list of all items in level

--- src/serialization/GAEtoJSON.java
+++ src/serialization/GAEtoJSON.java
@@ -10,24 +10,13 @@ public interface GAEtoJSON	{
 	/**
 	 * Called by GAE, updates database with changes from last time GAE saved data
 	 * Data passed are levels or settings (corresponding to files), and a list of the items per file to update
-	 * @param changes	editedItems is List<List<item>>. editedItems contains List<item> addedItems, List<item> changedItems, List<item> removedItems
+	 * @param changes	editedItems is a list of all GameObjects currently in authoring environment
 	 */
-	void update(Map<Level, List<List<GameObject>>> changes);
-		// for (Object file:filesToEdit)	{
-		// 	for (Object added:editedItems.get(0))	{
-		// 		// add new item to database
-		// 	}
-		// 	for (Object changed:editedItems.get(1))	{
-		// 		// find item in database by id, update it with new properties
-		// 	}
-		// 	for (Object removed:editedItems.get(2))	{
-		// 		// find item in database by id, remove it from database
-		// 	}
-		// }
+	void update(Map<Level, List<GameObject>> changes);
 
 	/**
 	 * Method that allows for changes to be reverted to last saved version of game, if user decides to entirely scrap their updats
 	 * @param gameName		name of game to load past save of
 	 */
-	List<Object> revertChanges(String gameName);
+	List<Object> revertChanges(Level level);
 }
---
+++ src/serialization/TextWriter.java
@@ -0,0 +1,123 @@
+package serialization;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TextWriter	{
+	public TextWriter(File level, List itemsInLevel)	{
+		try	{
+			FileWriter fw = new FileWriter(level);	// get number from level
+		
+			startFile(fw);
+			addtoWrite(fw, itemsInLevel);
+			endFile(fw);
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private Map<String, List<Object>> sortObjects(List<GameObject> objsToWrite)	{
+		Map<String, List<Object>> objsOrganized = new HashMap<>();	
+
+		for (Object obj:objsToWrite)	{
+			String oc = obj.getClass().getSimpleName();
+
+			if (!objsOrganized.containsKey(oc))	{
+				objsOrganized.put(oc, new ArrayList<Object>());
+			}
+			objsOrganized.get(oc).add(obj);
+		}
+
+		return objsOrganized;
+	}
+
+	private void addtoWrite(FileWriter fw, List<GameObject> items)	{
+		int entryIndex = 0;
+		Map<String, List<Object>> objsOrganized = sortObjects(items);
+		for (Map.Entry entry:objsOrganized.entrySet())	{
+			String aClass = (String)entry.getKey();
+			List<Object> classObjects = (List)entry.getValue();
+
+			startArray(fw, aClass);
+			writeArray(fw, classObjects);
+			closeArray(fw, entryIndex, objsOrganized.entrySet().size());
+			entryIndex++;
+		}
+	}
+
+	private void startFile(FileWriter fw)	{
+		try	{
+			fw.write(curlyBracketOpen);
+			fw.write(System.lineSeparator());
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private void startArray(FileWriter fw, String title)	{
+		try	{
+			fw.write(quote + title + quote + colon + bracketOpen);
+			fw.write(System.lineSeparator());
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private void closeArray(FileWriter fw, int entryIndex, int mapSize)	{
+		try	{
+			fw.write(bracketClose);
+			checkWriteComma(fw, entryIndex, mapSize);
+			fw.write(System.lineSeparator());
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private void writeArray(FileWriter fw, List toWrite)	{
+		try	{
+			for (Object obj:toWrite)	{
+				fw.write(ser.serialize(obj));
+				checkWriteComma(fw, toWrite.indexOf(obj), toWrite.size());
+				fw.write(System.lineSeparator());
+			}
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private void checkWriteComma(FileWriter fw, int index, int size)	{
+		if (index < size - 1)	{
+			try	{
+				fw.write(comma);
+			}
+			catch (IOException e)	{
+				error(e);
+			}
+		}
+	}
+
+	private void endFile(FileWriter fw)	{
+		try	{
+			fw.write(curlyBracketClose);
+			fw.close();
+		}
+		catch (IOException e)	{
+			error(e);
+		}
+	}
+
+	private void error(IOException e)	{
+		System.out.println("Could not write to file");
+	}
+}
