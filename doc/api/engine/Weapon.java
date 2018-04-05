9b991440ca7ae41e60dcce829fdfd49ced3d6335
Robert <rg167@duke.edu>
2018-04-04 22:11:47 -0400

    Interface Shells

--- doc/api/engine/Weapon.java
+++ doc/api/engine/Weapon.java
@@ -5,15 +5,9 @@ package api.engine;
  * @author Marcus
  *
  */
-public interface Weapon {
-	/**
-	 * Sets the attack method type for a weapon (ie. swing, stab, shoot, etc...)
-	 * @param method - the string representation of the desired attack method
-	 */
-	public abstract void setAttackMethod(String method);
-	
+public interface Weapon {	
 	/**
 	 * Causes the weapon to attack
 	 */
-	public abstract void attack();
+	public abstract void attack(GameEntity entity);
 }
--- src/engine/powerup/PowerUp.java
+++ src/engine/powerup/PowerUp.java
@@ -1,5 +1,19 @@
 package engine.powerup;
-
+/**
+ * Defines power ups that grant special effects on the player on contact for a set duration.
+ * @author Marcus Oertle and Robert Gitau
+ *
+ */
 public interface PowerUp {
-
+	/**
+	 * Sets the duration a power up lasts for
+	 * @param time - the duration time in milliseconds
+	 */
+	public abstract void setDuration(double time);
+	
+	/**
+	 * Sets the object that the PowerUp is interacting with
+	 * @param object - the object that the PowerUp is interacting with
+	 */
+	public abstract void setInteraction(Object object);
 }
