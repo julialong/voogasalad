acdd6e5235082bbf9b1c0d6641f836562c1e6b35
Marcus Oertle <mlo11@duke.edu>
2018-04-04 21:45:19 -0400

    commented physics and kinematics and coordinates classes

--- src/engine/physics/Coordinates.java
+++ src/engine/physics/Coordinates.java
@@ -9,6 +9,19 @@ public class Coordinates {
 	private double xCor;
 	private double yCor;
 	
+	/**
+	 * Construct Coordinates class without parameters, default to 0 for coordinate values
+	 */
+	public Coordinates() {
+		xCor = 0;
+		yCor = 0;
+	}
+	
+	/**
+	 * Construct Coordinates class with given parameters
+	 * @param x
+	 * @param y
+	 */
 	public Coordinates(double x, double y) {
 		xCor = x;
 		yCor = y;
@@ -28,5 +41,21 @@ public class Coordinates {
 	 */
 	public double getY() {
 		return yCor;
+	}
+	
+	/**
+	 * Sets the x coordinate
+	 * @param xC - (double) x coordinate
+	 */
+	public void setX(double xC) {
+		xCor = xC;
+	}
+	
+	/**
+	 * Sets the x coordinate
+	 * @param yC - (double) x coordinate
+	 */
+	public void setY(double yC) {
+		yCor = yC;
 	}
 }
--- src/engine/physics/Kinematics.java
+++ src/engine/physics/Kinematics.java
@@ -1,11 +1,34 @@
 package engine.physics;
 
+/**
+ * Kinematics object stores velocities, accelerations, and frictional constants that affect a game entity
+ * @author Marcus Oerte
+ *
+ */
 public class Kinematics {
 	private double xVel;
 	private double yVel;
 	private double xAcc;
 	private double yAcc;
+	private double frictionConstant = 10;
 	
+	/**
+	 * Constructs Kinematics object with 0 initial velocity/acceleration
+	 */
+	public Kinematics() {
+		xVel = 0;
+		yVel = 0;
+		xAcc = 0;
+		yAcc = 0;
+	}
+	
+	/**
+	 * Construct Kinematics object based on given parameters
+	 * @param xv - x velocity
+	 * @param yv - y velocity
+	 * @param xa - x acceleration
+	 * @param ya - y acceleration
+	 */
 	public Kinematics(double xv, double yv, double xa, double ya) {
 		xVel = xv;
 		yVel = yv;
@@ -13,19 +36,83 @@ public class Kinematics {
 		yAcc = ya;
 	}
 	
+	/**
+	 * Returns the x velocity
+	 * @return xVel
+	 */
 	public double getXVelocity() {
 		return xVel;
 	}
 	
+	/**
+	 * Sets the x velocity
+	 * @param xv
+	 */
+	public void setXVelocity(double xv) {
+		xVel = xv;
+	}
+	
+	/**
+	 * Returns the y velocity
+	 * @return yVel
+	 */
 	public double getYVelocity() {
 		return yVel;
 	}
 	
+	/**
+	 * Sets the y velocity
+	 * @param yv
+	 */
+	public void setYVelocity(double yv) {
+		yVel = yv;
+	}
+	
+	/**
+	 * Returns the x acceleration
+	 * @return xAcc
+	 */
 	public double getXAcceleration() {
 		return xAcc;
 	}
 	
+	/**
+	 * Sets the x acceleration
+	 * @param xa
+	 */
+	public void setXAcceleration(double xa) {
+		xAcc = xa;
+	}
+	
+	/**
+	 * Returns the y acceleration
+	 * @return yAcc
+	 */
 	public double getYAcceleration() {
 		return yAcc;
+	}
+	
+	/**
+	 * Sets the y acceleration
+	 * @param ya
+	 */
+	public void setYAcceleration(double ya) {
+		yAcc = ya;
+	}
+	
+	/**
+	 * Returns the frictional constant
+	 * @return frictionConstant
+	 */
+	public double getFrictionConstant() {
+		return frictionConstant;
+	}
+	
+	/**
+	 * Sets the frictional constant
+	 * @param fc
+	 */
+	public void setFrictionConstant(double fc) {
+		frictionConstant = fc;
 	}
 }
