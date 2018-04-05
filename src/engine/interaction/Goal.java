bc33a9ddecba70624129f8a51c5ad83179b62e7d
Robert <rg167@duke.edu>
2018-04-04 23:08:50 -0400

    Shells of classes

---
+++ src/engine/interaction/Goal.java
@@ -0,0 +1,15 @@
+package engine.interaction;
+/**
+ * If the target is a PlayerCharacter, the player moves to the next level.
+ * @author Robert Gitau
+ *
+ */
+public class Goal implements Interaction{
+
+	@Override
+	public void interact(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/engine/weapon/StabbingWeapon.java
@@ -0,0 +1,17 @@
+package engine.weapon;
+
+import engine.entity.GameEntity;
+/**
+ * A weapon that strikes Entities directly in front of the player
+ * @author Robert Gitau
+ *
+ */
+public class StabbingWeapon implements Weapon{
+
+	@Override
+	public void attack(GameEntity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
