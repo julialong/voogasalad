27b922f1b7bab9636c7a443962ff60a89e23ae76
Marcus Oertle <mlo11@duke.edu>
2018-04-04 22:57:49 -0400

    added skeletons for movement and behavior implementations

---
+++ src/engine/behavior/ReverseOnLedge.java
@@ -0,0 +1,16 @@
+package engine.behavior;
+
+/**
+ * Behavior implementation that causes entity to reverse direction when it reaches the edge of an object it is standing on
+ * @author Robert Gitau and Marcus Oertle
+ *
+ */
+public class ReverseOnLedge implements Behavior{
+
+	@Override
+	public void behave(Object target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
