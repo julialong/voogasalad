30ccf85f6b8bfe64eb2a33d2a7da8fd1f824d7cd
Marcus Oertle <mlo11@duke.edu>
2018-04-08 00:35:53 -0400

    updated moveup and movedown behaviors

--- src/engine/controls/MoveUp.java
+++ src/engine/controls/MoveUp.java
@@ -1,11 +1,14 @@
 package engine.controls;
 
 import engine.entity.GameEntity;
+import engine.movement.Flying;
 
 public class MoveUp extends Action {
 
-    @Override
-    public void execute(GameEntity entity){
-        entity.setYAcceleration(entity.getSpeedFactor());
-    }
+	@Override
+	public void execute(GameEntity entity){
+		if(entity.getMovementType() instanceof Flying) {
+			entity.setYAcceleration(entity.getSpeedFactor());
+		}
+	}
 }
