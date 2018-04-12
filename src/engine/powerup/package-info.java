73e8b336f60180334030bfdf4c45bd8dd0a6ddd5
Belanie Nagiel <bn51@duke.edu>
2018-04-12 14:28:36 -0400

    package info files

--- src/engine/powerup/package-info.java
+++ src/engine/powerup/package-info.java
@@ -2,4 +2,4 @@
 * Contains implementations of the PowerUp interface that allow for players to collect certain 
 * power ups to change some nature of their state and/or their interactions with other Entities.
  */
-package engine.powerups;
+package engine.powerup;
