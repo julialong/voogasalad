97865c8eb8fbf6b5bfa218084905a27ebad73be7
Dorian Barber <dpb20@duke.edu>
2018-04-13 22:28:57 -0400

    Developing Overview and styling

--- src/game_player/OverViewDriver.java
+++ src/game_player/OverViewDriver.java
@@ -3,27 +3,49 @@ package game_player;
 import authoring_environment.EditorWindow;
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * This class is designed to allow the user to either open
+ * the game authoring environment or the game chooser application
+ * depending on their choice.
+ *
+ * @Author Dorian Barber
+ */
 public class OverViewDriver extends Application {
 
+    /**
+     * Starts the main application
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("SuperSaladSquad");
         Scene scene = new Scene(getScene(primaryStage));
         scene.getStylesheets().add("../data/styling/overview.css");
+        primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
+    /**
+     * Creates the the user interface
+     * @return the pane to be displayed toward the user
+     *
+     * TODO: Refactor
+     */
     private Pane getScene(Stage stage){
         Pane pane = new Pane();
-        HBox container = new HBox();
+        VBox container = new VBox();
+        HBox buttonContainer = new HBox();
         Button gae = new Button("Game Authoring Environment");
         gae.setOnAction(event -> {
             new EditorWindow(stage);
@@ -34,8 +56,17 @@ public class OverViewDriver extends Application {
             new VoogaChooser(stage);
         });
 
-        container.getChildren().add(gae);
-        container.getChildren().add(gp);
+        buttonContainer.getChildren().add(gae);
+        buttonContainer.getChildren().add(gp);
+
+        Text title = new Text("Welcome to Super Salad Squad");
+        title.setFont(new Font(30));
+        Text t = new Text("Choose an application to open");
+
+        container.getChildren().add(title);
+        container.getChildren().add(t);
+        container.getChildren().add(buttonContainer);
+        container.getStyleClass().add("vbox");
         pane.getChildren().add(container);
         return pane;
     }
