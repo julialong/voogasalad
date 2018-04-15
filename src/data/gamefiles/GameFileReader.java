43b987d73edbb7f45c297f909e5098de3ddda155
Maya Messinger <maya.messinger@duke.edu>
2018-04-15 15:59:37 -0400

    Fix game file directory issue - nest symbol

--- src/data/gamefiles/GameFileReader.java
+++ src/data/gamefiles/GameFileReader.java
@@ -39,10 +39,10 @@ import engine.level.Level;
 public class GameFileReader implements JSONtoObject {
 
 	private static final String GAME_FOLDER = "./data/gameData";
-	private static final String NEST = "/";
 	private static final String JSON_EXTENSION = ".json";
 	private static final String RESOURCE_FILE = "data.resources/gameObjects";
 	private static final String SETTINGS = "Settings";
+	private String NEST = "/";
 	private String gameDirectory;
 	private File currentGame;
 	private File currentLevel;
@@ -57,7 +57,11 @@ public class GameFileReader implements JSONtoObject {
 	{
 		objectTypes= new HashMap<>();
 		createObjectToClassMap();
-		deserializer = new Serializer(); 
+		deserializer = new Serializer();
+
+		if (!System.getProperty("os.name").contains("Mac"))	{
+			NEST = "\\";
+		}
 	}
 	
 	/**
@@ -90,7 +94,7 @@ public class GameFileReader implements JSONtoObject {
 	private void retrieveCurrentGame(String gameName)
 	{
 		gameDirectory = GAME_FOLDER + NEST + gameName;
-		currentGame = new File(gameDirectory); 
+		currentGame = new File(gameDirectory); 	
 	}
 	
 	/**
