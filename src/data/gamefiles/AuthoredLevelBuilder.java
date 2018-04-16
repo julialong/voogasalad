2b825f74322aca2d74c5579e5bafd183f1c96c82
Belanie Nagiel <bn51@duke.edu>
2018-04-16 16:04:52 -0400

    additions to authoredlevelbuilder

--- src/data/gamefiles/AuthoredLevelBuilder.java
+++ src/data/gamefiles/AuthoredLevelBuilder.java
@@ -1,8 +1,27 @@
 package data.gamefiles;
 
+import java.io.File;
+
+import authoring_environment.game_elements.AuthoredLevel;
+import engine.level.Level;
+
 public class AuthoredLevelBuilder {
 	
-	public AuthoredLevelBuilder() {
+	private File levelFile;
+	
+	public AuthoredLevelBuilder(File level) 
+	{
+		levelFile = level;
+	}
+	
+	public AuthoredLevel buildAuthoredLevel()
+	{
+		LevelBuilder levelBuilder = new LevelBuilder(levelFile);
+		Level levelForAuthoredLevel = levelBuilder.buildLevel();
 		
+		//get the scrolling grid
+		
+		//make a new AuthoredLevel object
+		return null;
 	}
 }
