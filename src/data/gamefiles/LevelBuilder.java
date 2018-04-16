fdd6306f9242264886a2ed32839584d5d93eb06c
Belanie Nagiel <bn51@duke.edu>
2018-04-16 14:24:11 -0400

    created level builder

--- src/data/gamefiles/LevelBuilder.java
+++ src/data/gamefiles/LevelBuilder.java
@@ -1,19 +1,29 @@
 package data.gamefiles;
 
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import data.serialization.Serializer;
+import engine.entity.GameEntity;
+import engine.level.BasicLevel;
+import engine.level.Level;
 
 public class LevelBuilder {
 
 	private static final String RESOURCE_FILE = "data.resources/gameObjects";
 	private Map<String,Class<?>> objectTypes;
 	private Serializer deserializer; 
-	private File level;
+	private File levelFile;
 	
 	public LevelBuilder(File level) 
 	{
@@ -21,7 +31,7 @@ public class LevelBuilder {
 		createObjectToClassMap();
 		deserializer = new Serializer();
 		
-		this.level = level;
+		levelFile = level;
 	}
 	
 	/**
@@ -46,5 +56,64 @@ public class LevelBuilder {
 		}
 	}
 	
+	public Level buildLevel()
+	{
+		BasicLevel level = new BasicLevel();
+		try 
+		{
+			JsonParser jsonParser = new JsonParser();
+			JsonObject  jobject = jsonParser.parse(new FileReader(levelFile)).getAsJsonObject();
+			addMetaData(level, jobject);
+			addGameObjects(level,jobject);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return level;	
+	}
+	
+	private void addMetaData(Level level, JsonObject jobject)
+	{
+		String levelName = jobject.get("name").getAsString();
+		int id = jobject.get("id").getAsInt();
+		
+		level.setName(levelName);
+		level.setID(id);
+	}
+	
+	private void addGameObjects(Level level, JsonObject jobject)
+	{
+		List<GameEntity> gameObjects = new ArrayList<>();
+		
+		for(String objectType: objectTypes.keySet())
+		{
+			if(jobject.has(objectType))
+			{
+				JsonArray jarray = jobject.getAsJsonArray(objectType);
+				for(int i = 0; i < jarray.size(); i++)
+				{
+					gameObjects.add((GameEntity) convertToObject(jarray.get(i).getAsJsonObject(), objectType));
+				}
+			}
+		}
+		level.setObjects(gameObjects);
+	}
+
+	/**
+	 * Given the JSON object that will be converted into the game object
+	 * and the type of the game object, returns the GameObject object for 
+	 * the JSON text.
+	 * 
+	 * @param toConvert
+	 * @param objectType
+	 * @return
+	 */
+	private Object convertToObject(JsonObject toConvert, String objectType)
+	{
+		JsonObject j = toConvert;
+		Object converted = deserializer.deserialize(j.toString(), objectTypes.get(objectType));
+		return converted;
+	}
 	
 }
--- src/data/gamefiles/NewGameFileReader.java
+++ src/data/gamefiles/NewGameFileReader.java
@@ -1,6 +1,7 @@
 package data.gamefiles;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -43,7 +44,6 @@ public class NewGameFileReader implements JSONtoGAE, JSONtoGP{
 	{
 		String gameDirectory = GAME_FOLDER + NEST + gameName;
 		return new File(gameDirectory + NEST + level + JSON_EXTENSION);
-//		currentLevel = new File(gameDirectory + "/" + "Default.json");
 	}
 	
 	/**
@@ -57,42 +57,110 @@ public class NewGameFileReader implements JSONtoGAE, JSONtoGP{
 		return new File(gameDirectory + NEST + SETTINGS + JSON_EXTENSION);
 	}
 	
+	/**
+	 * This method will return the list of Level objects created from the level
+	 * files for a particular game. This will locate the folder for the specific
+	 * game and load the different level files associated with that folder.
+	 * 
+	 * @param gameName
+	 * @return 
+	 */
 	@Override
 	public List<Level> loadCompleteGame(String gameName) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Level> completeGame = new ArrayList<>();
+		File currentGame = retrieveCurrentGame(gameName);
+		File[] gameFiles = currentGame.listFiles();
+		for(File gameFile: gameFiles)
+		{
+				int index = gameFile.toString().lastIndexOf(NEST) + 1;
+				int endIndex = gameFile.toString().lastIndexOf(JSON_EXTENSION);
+				String levelName = gameFile.toString().substring(index,endIndex).trim();
+				if(!levelName.equals(SETTINGS))
+				{
+					completeGame.add(loadLevel(gameName, levelName));
+				}		
+		}
+		return completeGame;
 	}
 
+	/**
+	 * This will load a specific level within a game based on the game name 
+	 * and the name of the level. Will return the Level object associated with 
+	 * that level file.
+	 * 
+	 * @param gameName
+	 * @param levelName
+	 * @return
+	 */
 	@Override
 	public Level loadLevel(String gameName, String levelName) {
-		// TODO Auto-generated method stub
-		return null;
+		File currentLevel = retrieveLevel(gameName, levelName);
+		LevelBuilder levelBuilder = new LevelBuilder(currentLevel);
+		return levelBuilder.buildLevel();
 	}
 
+	/**
+	 * This will load the different settings associated with a game, i.e the 
+	 * description of the game and whether or not it is ready to be played.
+	 * Returns a map of the setting to the value.
+	 * 
+	 * @param gameName
+	 * @return
+	 */
 	@Override
 	public Map<String, String> loadSettings(String gameName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * This returns a Map of the names of the ready to play games and their
+	 * descriptions.
+	 * 
+	 * @return
+	 */
 	@Override
 	public Map<String, String> getGameNames() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * This method will return the list of AuthoredLevel objects created from the level
+	 * files for a particular game. This will locate the folder for the specific
+	 * game and load the different level files associated with that folder. This will 
+	 * allow the game authoring environment to do continued editing on a game.
+	 * 
+	 * @param gameName
+	 * @return 
+	 */
 	@Override
 	public List<AuthoredLevel> loadCompleteAuthoredGame(String gameName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * This will load one of the stray levels that an author can
+	 * add to his or her game in the game authoring environment.
+	 * 
+	 * @param levelName
+	 * @return
+	 */
 	@Override
 	public AuthoredLevel loadAuthoredLevel(String levelName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * This will load the author settings for a specific author in the 
+	 * game authoring environment. It will return the map of image paths 
+	 * to custom game entities. 
+	 * 
+	 * @param author
+	 * @return
+	 */
 	@Override
 	public Map<String, GameEntity> loadAuthorCustomObjects(String author) {
 		// TODO Auto-generated method stub
