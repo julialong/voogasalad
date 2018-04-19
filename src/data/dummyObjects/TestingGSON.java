167c81f8dd8463faee441ed4b197716534f4899b
Belanie Nagiel <bn51@duke.edu>
2018-04-18 21:56:27 -0400

    fixed file retriever

--- src/data/dummyObjects/TestingGSON.java
+++ src/data/dummyObjects/TestingGSON.java
@@ -16,9 +16,9 @@ public class TestingGSON {
 	public static void main(String[] args) 
 	{
 		FileRetriever fr = new FileRetriever();
-		System.out.println(fr.retrieveAllGamePaths());
-		System.out.println(fr.retrieveCurrentGamePath("Game1"));
-		System.out.println(fr.retrieveLevel("Game3", "samplelevel"));
+//		System.out.println(fr.retrieveAllGamePaths());
+//		System.out.println(fr.retrieveCurrentGamePath("Game1"));
+//		System.out.println(fr.retrieveLevel("Game3", "samplelevel"));
 		
 		GPGameFileReader gp = new GPGameFileReader();
 		System.out.println(gp.getGameNames());
