461959dbc88cc6755dcac041eb5bade1b7d2d1d3
Maya Messinger <maya.messinger@duke.edu>
2018-04-21 13:50:21 -0400

    Deprecation and changing file nest value

--- src/data/gamefiles/GEtoJSON.java
+++ src/data/gamefiles/GEtoJSON.java
@@ -9,11 +9,18 @@ import engine.level.Level;
  */
 public interface GEtoJSON	{
 	/**
-	 * Called by Game Engine, updates database with data as it is currently in the play environment
-	 * Only need to save state of current level, because player will either have completed past level (making it unnecessary to save)
-	 * or has not reached further level (unnecessary to save as new file, file exists in game data already)
+	 * Called by Game Engine, updates database with data for player as it is currently in the play environment
 	 * @param player		name of player
 	 * @param levels		list of levels to save
 	 */
 	void saveData(String player, List<Level> levels);
+
+	/**
+	 * Called by Game Engine, updates database with data as it is currently in the play environment
+	 * Only need to save state of current level, because player will either have completed past level (making it unnecessary to save)
+	 * or has not reached further level (unnecessary to save as new file, file exists in game data already)
+	 * @param level		level to save
+	 */
+	@Deprecated
+	void saveData(Level levels);
 }
--- src/data/gamefiles/GameFileWriter.java
+++ src/data/gamefiles/GameFileWriter.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 
+import data.fileReading.GAEGameFileReader;
 import authoring_environment.game_elements.AuthoredLevel;
 import authoring_environment.grid.ScrollingGrid;
 import data.serialization.TextWriter;
@@ -22,7 +23,7 @@ import engine.level.Level;
 public class GameFileWriter implements GAEtoJSON, GEtoJSON	{
 	private static final String GAMEDATA = "./data/gameData/";
 	private static final String LEVELDATA = "./data/levelData/";
-	private static final String NEST = "/";
+	private static final String NEST = File.separator;
 	private static final String SETTINGS = "Settings";
 	private static final String EXTENSION = ".json";
 
@@ -81,6 +82,16 @@ public class GameFileWriter implements GAEtoJSON, GEtoJSON	{
 	 * Saves state of level being played, for use with checkpoints
 	 * @param level			name of level to save
 	 */
+	@Override
+	@Deprecated
+	public void saveData(Level level)	{
+		new TextWriter(new AuthoredLevel(level, new ScrollingGrid()), getLevel(level));
+	}
+
+	/**
+	 * Saves state of level being played, for use with checkpoints
+	 * @param level			name of level to save
+	 */
 	public void saveData(AuthoredLevel level)	{
 		new TextWriter(level, getLevel(level.getLevel()));
 	}
@@ -102,10 +113,23 @@ public class GameFileWriter implements GAEtoJSON, GEtoJSON	{
 	 * @return	new, replacement instance of level
 	 */
 	@Override
-	public Level revertChanges(AuthoredLevel level)	{
-		GameFileReader reader = new GameFileReader();
+	@Deprecated
+	public Level revertChanges(Level level)	{
+		GAEGameFileReader reader = new GAEGameFileReader();
 		String levelName = level.getName();
-		return reader.loadLevel(gameName, levelName);
+		return reader.loadAuthoredGameLevel(gameName, levelName).getLevel();
+	}
+
+	/**
+	 * Cancels any edits made to a game since last save
+	 * @param level	level to cancel changes to
+	 * @return new, replacement instance of level
+	 */
+	@Override
+	public AuthoredLevel revertChanges(AuthoredLevel level)	{
+		GAEGameFileReader reader = new GAEGameFileReader();
+		String levelName = level.getName();
+		return reader.loadAuthoredGameLevel(gameName, levelName);
 	}
 
 	/**
