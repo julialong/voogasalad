a33cdf897024f570dd63ba75e974a77fa58b3b1d
Judith Sanchez Sorian <js640@duke.edu>
2018-04-22 17:10:58 -0400

    basic settings

--- src/authoring_environment/editor_windows/buttons/SaveSettingsButton.java
+++ src/authoring_environment/editor_windows/buttons/SaveSettingsButton.java
@@ -3,10 +3,7 @@ package authoring_environment.editor_windows.buttons;
 import authoring_environment.editor_windows.CreatorView;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * The Save Settings Button allows the user to save the 
@@ -17,28 +14,25 @@ import javafx.scene.text.Text;
 public class SaveSettingsButton extends Button{
 
     private static final String SAVE_SETTINGS = "Save Game Settings";
-    private static final String GAME_NAME = "Game name: ";
-    private static final int LARGE_FONT = 20;
-    private Color textColor = Color.BLACK;
     private String gameName;
+    private String description;
 
     /**
      * Creates a new SaveGameButton
      */
-    public SaveSettingsButton(CreatorView window, Pane pane, TextField fileName) {
+    public SaveSettingsButton(CreatorView window, Stage stage, TextField gameNameField, TextField descriptionField) {
         super(SAVE_SETTINGS);
-        this.setOnAction(e -> saveSettings(window, pane, fileName));
+        this.setOnAction(e -> saveSettings(window, stage, gameNameField, descriptionField));
     }
 
-    private void saveSettings(CreatorView myWindow, Pane pane, TextField fileName) {
-    		pane.getChildren().removeAll(fileName);
-        gameName = fileName.getText();
-        Text name = new Text(gameName + GAME_NAME );
-        name.setFont(new Font(LARGE_FONT));
-        name.setFill(textColor);
-        pane.getChildren().add(name);
+    private void saveSettings(CreatorView myWindow, Stage stage, TextField gameNameField, TextField descriptionField) {
+        gameName = gameNameField.getText();
+        description = descriptionField.getText();
     		myWindow.getGame().rename(gameName);
-    		System.out.println(myWindow.getGame().getName());
+    		myWindow.getGame().setDescription(description);
+    		System.out.println(myWindow.getGame().getName() + "\n");
+    		System.out.println(myWindow.getGame().getDescription());
+    		stage.close();
     	
     }
 
