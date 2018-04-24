d8b9fa459f906f4004802f4dfc5d7dd99f146b0a
Belanie Nagiel <bn51@duke.edu>
2018-04-23 21:30:07 -0400

    trying to get out the bugs

--- src/data/firebase/FirebaseAuthentication.java
+++ src/data/firebase/FirebaseAuthentication.java
@@ -1,12 +1,15 @@
 package data.firebase;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.google.auth.oauth2.GoogleCredentials;
+
 import com.google.firebase.*;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,7 +35,8 @@ public class FirebaseAuthentication {
 		// Fetch the service account key JSON file content
 				try
 				{
-					FileInputStream serviceAccount = new FileInputStream("./data/firebase/serviceAccountKey.json");
+					FileInputStream serviceAccount = new FileInputStream("./data/firebase/serviceAccountKey.json");				
+					
 					FirebaseOptions options = new FirebaseOptions.Builder()
 						    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
 						    .setDatabaseUrl("https://voogasalad-supersaladsquad.firebaseio.com")
@@ -40,6 +44,26 @@ public class FirebaseAuthentication {
 					
 					// Initialize the app with a service account, granting admin privileges
 					FirebaseApp.initializeApp(options);
+					
+					FirebaseDatabase db = FirebaseDatabase.getInstance();
+					DatabaseReference ref = db.getReference("restricted_access/secret_document");
+					
+					
+					
+					ref.addListenerForSingleValueEvent(new ValueEventListener() {
+						
+					  @Override
+					  public void onDataChange(DataSnapshot dataSnapshot) {
+					    Object document = dataSnapshot.getValue();
+					    System.out.println(document);
+					  }
+				
+					  @Override
+					  public void onCancelled(DatabaseError error) {
+					  }
+					});	
+					
+					return ref;
 				}
 				catch(IOException e)
 				{
@@ -47,25 +71,7 @@ public class FirebaseAuthentication {
 				}
 
 				// As an admin, the app has access to read and write all data, regardless of Security Rules
-				FirebaseDatabase db = FirebaseDatabase.getInstance();
-				DatabaseReference ref = db.getReference("restricted_access/secret_document");
-				
-				
-				
-				ref.addListenerForSingleValueEvent(new ValueEventListener() {
-					
-				  @Override
-				  public void onDataChange(DataSnapshot dataSnapshot) {
-				    Object document = dataSnapshot.getValue();
-				    System.out.println(document);
-				  }
-			
-				  @Override
-				  public void onCancelled(DatabaseError error) {
-				  }
-				});	
-				
-				return ref;
+				return null;
 	}
 	
 	public void testAdd()
