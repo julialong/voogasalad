38cd0dd7648a45118ff6e18f6109243024ea65b2
Judith Sanchez Sorian <js640@duke.edu>
2018-04-25 00:17:03 -0400

    game buttons

--- src/authoring_environment/editor_windows/GameChooser.java
+++ src/authoring_environment/editor_windows/GameChooser.java
@@ -1,6 +1,8 @@
 package authoring_environment.editor_windows;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -22,12 +24,18 @@ public class GameChooser {
     private Stage myStage;
     private Pane myRoot;
     private Scene myScene;
-    private List<Text> myOptions;
+    private List<String> gameNames;
+    private List<HBox> rows;
 
     private static final String CSS = "GAE.css";
     private static final String LOAD_GAME = "Load Game";
     private static final String CHOOSE = "Choose a game to edit:";
+    private static final int BUTTONS_PER_ROW = 5;
+    private static final int BUTTON_SIZE = 50;
 
+    /**
+     * 
+     */
     public GameChooser() {
         myStage = new Stage();
         myRoot = new VBox();
@@ -38,9 +46,9 @@ public class GameChooser {
         myStage.setTitle(LOAD_GAME);
         myStage.show();
         myStage.centerOnScreen();
-        List<String> names = fetchGameNames();
-        addOptions(names);
-        myRoot.getChildren().addAll(myOptions);
+        gameNames = fetchGameNames();
+        createHBoxes();
+        addButtons();
     }
 
     private void addFields() {
@@ -50,7 +58,7 @@ public class GameChooser {
     }
     
     private List<String> fetchGameNames() {
-    		List<String> gameNames= new ArrayList<String>();
+    		gameNames= new ArrayList<String>();
     		FileRetriever fileRetriever = new FileRetriever();
     		List<String> gamePaths= fileRetriever.retrieveAllGamePaths();
     		for(String gamePath : gamePaths) {
@@ -59,11 +67,24 @@ public class GameChooser {
     		return gameNames;
     }
     
-    private void addOptions(List<String> gameNames) {
-    		myOptions= new ArrayList<Text>();
-    		for(String gameName: gameNames) {
-    			myOptions.add(new Text(gameName));
-    			System.out.println(gameName);
+    private void createHBoxes() {
+    		rows = new ArrayList<HBox>();
+    		for(int i=0; i < gameNames.size()/BUTTONS_PER_ROW; i++){
+    			rows.add(new HBox());
+    		}
+    		myRoot.getChildren().addAll(rows);
+    }
+    
+    private void addButtons() {
+    		for(int i =0; i< rows.size(); i++) {
+    			HBox box = rows.get(i);
+    			for(int j=0; j<BUTTONS_PER_ROW; j++) {
+    				Button button = new Button(gameNames.get(i*BUTTONS_PER_ROW + j));
+    				button.setMinSize(BUTTON_SIZE, BUTTON_SIZE);
+    				box.getChildren().add(button);
+    				
+    			}
+    			
     		}
     }
 
