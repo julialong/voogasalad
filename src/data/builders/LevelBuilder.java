40207d218b73922863315d955de9b8b82b817b72
Belanie Nagiel <bn51@duke.edu>
2018-04-28 17:33:34 -0400

    fixed errors from game engine merge

--- src/data/builders/LevelBuilder.java
+++ src/data/builders/LevelBuilder.java
@@ -72,6 +72,7 @@ public class LevelBuilder {
 			String objectName = objectNames.nextElement();
 			try 
 			{
+				System.out.println(objectName);
 				Class<?> objectClass = Class.forName(gameObjects.getString(objectName));
 				objectTypes.put(objectName, objectClass);
 			} 
