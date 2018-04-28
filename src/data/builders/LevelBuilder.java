162b95c1b86fd46fb3d6a4fc6d52bbc7ebca5954
Maya Messinger <maya.messinger@duke.edu>
2018-04-27 21:49:54 -0400

    Re-add thing for merge conflict

--- src/data/builders/LevelBuilder.java
+++ src/data/builders/LevelBuilder.java
@@ -117,6 +117,7 @@ public class LevelBuilder {
 	private void addMetaData(Level level, JsonObject jobject)
 	{
 		String levelName = jobject.get(NAME).getAsString();
+		levelID = jobject.get(ID).getAsInt();
 		
 		level.setName(levelName);
 //		level.setID(id);
