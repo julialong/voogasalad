da5a35d24be210eb669ceb3312994d8732a15628
Marcus <mlo11@duke.edu>
2018-04-28 20:19:05 -0400

    AOEWeapon base

--- src/engine/weapon/AOEWeapon.java
+++ src/engine/weapon/AOEWeapon.java
@@ -1,7 +1,10 @@
 package engine.weapon;
 
+import engine.entity.Block;
 import engine.entity.GameEntity;
 import engine.entity.GameObject;
+import engine.interaction.HarmTarget;
+import engine.physics.DetectCollision;
 
 /**
  * A weapon that can hit in a circle around the user
@@ -9,11 +12,32 @@ import engine.entity.GameObject;
  *
  */
 public class AOEWeapon extends GameObject implements Weapon{
-
+	private Block hitBox = new Block();
+	private GameEntity weaponHolder;
+	private DetectCollision collisionDetector = new DetectCollision();
+	private HarmTarget dealDamage = new HarmTarget();
+	private double xSize;
+	private double ySize;
+	
+	public AOEWeapon(GameEntity entity){
+		weaponHolder = entity;
+		xSize = weaponHolder.getSizeX();
+		ySize = weaponHolder.getSizeY();
+		hitBox.setSizeX(xSize + 1.5*xSize);
+		hitBox.setSizeY(ySize + 1.5*xSize);
+	}
+	
 	@Override
 	public void attack(GameEntity entity) {
-		// TODO Auto-generated method stub
-		
+		double xPos = weaponHolder.getPosition()[0];
+		double yPos = weaponHolder.getPosition()[1];
+		hitBox.setX(xPos - hitBox.getSizeX());
+		hitBox.setY(yPos + hitBox.getSizeY());
+		//for(GameEntity entity : LIST_OF_ENTITIES){
+			if(!collisionDetector.detect(hitBox, entity).equals("none")){
+				dealDamage.interact(hitBox, entity);
+			}
+		//}
 	}
 
 }
