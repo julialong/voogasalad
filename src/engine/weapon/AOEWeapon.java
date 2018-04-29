07ab1a03945798dacb2408224e82243cadfa5942
Marcus <mlo11@duke.edu>
2018-04-28 20:40:05 -0400

    list now accessible hopefully

--- src/engine/weapon/AOEWeapon.java
+++ src/engine/weapon/AOEWeapon.java
@@ -1,9 +1,12 @@
 package engine.weapon;
 
+import java.util.ArrayList;
+
 import engine.entity.Block;
 import engine.entity.GameEntity;
 import engine.entity.GameObject;
 import engine.interaction.HarmTarget;
+import engine.level.Level;
 import engine.physics.DetectCollision;
 
 /**
@@ -14,13 +17,16 @@ import engine.physics.DetectCollision;
 public class AOEWeapon extends GameObject implements Weapon{
 	private Block hitBox = new Block();
 	private GameEntity weaponHolder;
+	private Level level;
 	private DetectCollision collisionDetector = new DetectCollision();
 	private HarmTarget dealDamage = new HarmTarget();
+	private ArrayList<GameEntity> listOfEntities = new ArrayList<>();
 	private double xSize;
 	private double ySize;
 	
-	public AOEWeapon(GameEntity entity){
+	public AOEWeapon(GameEntity entity, Level level){
 		weaponHolder = entity;
+		this.level = level;
 		xSize = weaponHolder.getSizeX();
 		ySize = weaponHolder.getSizeY();
 		hitBox.setSizeX(xSize + 1.5*xSize);
@@ -33,11 +39,12 @@ public class AOEWeapon extends GameObject implements Weapon{
 		double yPos = weaponHolder.getPosition()[1];
 		hitBox.setX(xPos - hitBox.getSizeX());
 		hitBox.setY(yPos + hitBox.getSizeY());
-		//for(GameEntity entity : LIST_OF_ENTITIES){
+		listOfEntities = (ArrayList<GameEntity>) level.getObjects();
+		for(GameEntity entity : listOfEntities){
 			if(!collisionDetector.detect(hitBox, entity).equals("none")){
 				dealDamage.interact(hitBox, entity);
 			}
-		//}
+		}
 	}
 
 }
--- src/engine/weapon/SwingingWeapon.java
+++ src/engine/weapon/SwingingWeapon.java
@@ -1,9 +1,12 @@
 package engine.weapon;
 
+import java.util.ArrayList;
+
 import engine.entity.Block;
 import engine.entity.GameEntity;
 import engine.entity.GameObject;
 import engine.interaction.HarmTarget;
+import engine.level.Level;
 import engine.physics.DetectCollision;
 /**
  * A Weapon that hits in an arc in front of the Player
@@ -13,13 +16,16 @@ import engine.physics.DetectCollision;
 public class SwingingWeapon extends GameObject implements Weapon{
 	private Block hitBox = new Block();
 	private GameEntity weaponHolder;
+	private Level level;
 	private DetectCollision collisionDetector = new DetectCollision();
 	private HarmTarget dealDamage = new HarmTarget();
+	private ArrayList<GameEntity> listOfEntities = new ArrayList<>();
 	private double xSize;
 	private double ySize;
 	
-	public SwingingWeapon(GameEntity entity){
+	public SwingingWeapon(GameEntity entity, Level level){
 		weaponHolder = entity;
+		this.level = level;
 		xSize = weaponHolder.getSizeX();
 		ySize = weaponHolder.getSizeY();
 		hitBox.setSizeX(2*xSize);
@@ -32,11 +38,12 @@ public class SwingingWeapon extends GameObject implements Weapon{
 		double yPos = weaponHolder.getPosition()[1];
 		hitBox.setX(xPos);
 		hitBox.setY(yPos);
-		//for(GameEntity entity : LIST_OF_ENTITIES){
+		listOfEntities = (ArrayList<GameEntity>) level.getObjects();
+		for(GameEntity entity : listOfEntities){
 			if(!collisionDetector.detect(hitBox, entity).equals("none")){
 				dealDamage.interact(hitBox, entity);
 			}
-		//}
+		}
 	}
 
 }
