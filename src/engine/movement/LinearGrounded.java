83bffbfae5d332a11baa8c4b236fe6ca5e52d5ba
Robert <rg167@duke.edu>
2018-04-29 19:27:30 -0400

    Clean up time

--- src/engine/movement/LinearGrounded.java
+++ src/engine/movement/LinearGrounded.java
@@ -1,35 +1,33 @@
 package engine.movement;
 
-import engine.entity.GameEntity;
 import engine.physics.Kinematics;
 import engine.physics.Physics;
 
 /**
- * Movement implementation for entities that remain on the ground and are affected by gravity but move with constant velocity
+ * Movement implementation for entities that remain on the ground and are
+ * affected by gravity but move with constant velocity
+ * 
  * @author Robert Gitau and Marcus Oertle
  *
  */
 public class LinearGrounded implements Movement {
-	//private Grounded grounded = new Grounded();
 	private Physics physics = new Physics();
-	
-    @Override
-    public Kinematics update(Kinematics k, double xVelocityLimit, double yVelocityLimit){
-    	//return grounded.update(k, xVelocityLimit, yVelocityLimit);
-    	Kinematics kFinal = physics.applyPhysics(k, true, false);
-        if (kFinal.getXVelocity() > xVelocityLimit){
-            kFinal.setXVelocity(xVelocityLimit);
-        }
-        if(kFinal.getXVelocity() < -xVelocityLimit){
-        	kFinal.setXVelocity(-xVelocityLimit);
-        }
-        if (kFinal.getYVelocity() > yVelocityLimit){
-            kFinal.setYVelocity(yVelocityLimit);
-        }
-        if(kFinal.getYVelocity() < -yVelocityLimit){
-        	kFinal.setYVelocity(-yVelocityLimit);
-        }
-        //System.out.println("X velocity = " + kFinal.getXVelocity());
-        return kFinal;
-    }
+
+	@Override
+	public Kinematics update(Kinematics k, double xVelocityLimit, double yVelocityLimit) {
+		Kinematics kFinal = physics.applyPhysics(k, true, false);
+		if (kFinal.getXVelocity() > xVelocityLimit) {
+			kFinal.setXVelocity(xVelocityLimit);
+		}
+		if (kFinal.getXVelocity() < -xVelocityLimit) {
+			kFinal.setXVelocity(-xVelocityLimit);
+		}
+		if (kFinal.getYVelocity() > yVelocityLimit) {
+			kFinal.setYVelocity(yVelocityLimit);
+		}
+		if (kFinal.getYVelocity() < -yVelocityLimit) {
+			kFinal.setYVelocity(-yVelocityLimit);
+		}
+		return kFinal;
+	}
 }
--- src/engine/weapon/Weapon.java
+++ src/engine/weapon/Weapon.java
@@ -1,6 +1,5 @@
 package engine.weapon;
 
-import engine.entity.GameEntity;
 /**
  * Defines something used by an Enemy or PlayerCharacter to effect harm on a certain GameEntity in collision with the weapon.
  * @author Robert Gitau and Marcus Oertle
