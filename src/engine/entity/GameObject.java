4a110ba18bbef2183aef0e9e24a3d1b048c22489
Julia Long <julia.long@duke.edu>
2018-04-29 12:50:55 -0400

    Undo small testing change

--- src/engine/entity/GameObject.java
+++ src/engine/entity/GameObject.java
@@ -134,9 +134,7 @@ public abstract class GameObject implements GameEntity{
 	public void interact(GameEntity source, GameEntity target, String direction) {
 		interactionsMap.put(target, direction);
 		for(Interaction i : interactionList) {
-			if (i != null) {
-				i.interact(source, target);
-			}
+			i.interact(source, target);
 		}
 	}
 	
