7f71ab2700494a076c7e26a71dfe3e6aca628772
Judith Sanchez Sorian <js640@duke.edu>
2018-04-30 00:20:37 -0400

    copy behavior

--- src/authoring_environment/attribute_editor/InteractionAttribute.java
+++ src/authoring_environment/attribute_editor/InteractionAttribute.java
@@ -1,6 +1,11 @@
 package authoring_environment.attribute_editor;
 
+import java.util.List;
+
+import javafx.collections.FXCollections;
 import javafx.scene.Group;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 
 public class InteractionAttribute extends Attribute {
@@ -15,8 +20,19 @@ public class InteractionAttribute extends Attribute {
 	 @Override
 	 
 	 public VBox setupInputs(String attributeType) {
-		// TODO Auto-generated method stub
-		return null;
+		 VBox container = new VBox();
+	        List<String> behaviorAttributesOptions = super.loadAttributes(attributeType);
+	        ObservableList<String> options = FXCollections.observableArrayList(behaviorAttributesOptions);
+	        ListView<String> chooser = new ListView<>(options);
+	        container.getChildren().add(chooser);
+	        chooser.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	        chooser.setOnMouseClicked(event -> {
+	            List<String> selectedItems = chooser.getSelectionModel().getSelectedItems();
+	            for(String option : selectedItems){
+	                container.getChildren().add(getOptionContents(option));
+	            }
+	        });
+	        return container;
 	}
 
 }
